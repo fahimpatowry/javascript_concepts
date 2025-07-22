7fa1711b329eff97dd4f3d52d25100bdd361cf0d
MD Fahim Patowry <70106181+fahimpatowry@users.noreply.github.com>
2025-07-22 18:10:55 +0000

    string, number_and_math

---
+++ 01_basics/05_string.js
@@ -0,0 +1,48 @@
+
+// string interpolation
+const name1 = 'string interpolation'
+console.log(`this is ${name1}, you can print it`)
+
+// 
+const Myname = 'Fahim Patowry'
+console.log(Myname.substring(0, 4)) // substring coan't take nagative valus
+console.log(Myname.slice(-3, 4)) // slice an take nagative value
+
+/* vidoe note start */
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
+
+/* vidoe note end */
