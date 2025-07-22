52fe730ff271fc084f899a3fb65d1e33d31ccce7
MD Fahim Patowry <70106181+fahimpatowry@users.noreply.github.com>
2025-07-22 19:07:04 +0000

    date time

---
+++ 01_basics/07_date_time.js
@@ -0,0 +1,41 @@
+
+// for compare date we can conver it in mili second
+const dateOne = new Date('01-24-2004')
+const dateTow = new Date()
+// console.log(dateOne.getTime())
+// console.log(dateTow.getTime())
+console.log(dateTow.getTime() > dateOne.getTime())
+
+// when we use getMonth it will star with 0(0-11)
+
+/* vidoe note start */
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    
+})
+/* vidoe note end */
